f4d29ba8041475192919b50522b08857e0c0bd5f
souvik1982 <souvik1982@gmail.com>
2013-09-23 03:37:06 -0500

    added b_KinFit

---
+++ StandardSelection/MMMM_nominal/b_KinFit/processPartitioning_Data.sh
@@ -0,0 +1,5 @@
+root -l -b -q '../../HbbHbb_Partitioning.cc++("BJetPlusX_Run2012B-13Jul2012-v1_EDMNtuple_V42_ProcV1_Skim", 90., 1)'
+root -l -b -q '../../HbbHbb_Partitioning.cc++("BJetPlusX_Run2012C-24Aug2012-v2_EDMNtuple_V42_ProcV1_Skim", 90., 1)'
+root -l -b -q '../../HbbHbb_Partitioning.cc++("BJetPlusX_Run2012C-PromptReco-v2_EDMNtuple_V42_ProcV1_Skim", 90., 1)'
+root -l -b -q '../../HbbHbb_Partitioning.cc++("BJetPlusX_Run2012D-PromptReco-v1_EDMNtuple_V42_ProcV1_Skim", 90., 1)'
+hadd -f Histograms_BJetPlusX_Run2012BCD_Skim.root Histograms_BJetPlusX_Run2012B-13Jul2012-v1_EDMNtuple_V42_ProcV1_Skim.root Histograms_BJetPlusX_Run2012C-24Aug2012-v2_EDMNtuple_V42_ProcV1_Skim.root Histograms_BJetPlusX_Run2012C-PromptReco-v2_EDMNtuple_V42_ProcV1_Skim.root Histograms_BJetPlusX_Run2012D-PromptReco-v1_EDMNtuple_V42_ProcV1_Skim.root
