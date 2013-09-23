aabab4fe208859a7b265a7b60a486dfb60f06bd2
souvik1982 <souvik1982@gmail.com>
2013-09-23 12:40:36 -0500

    PDFs script

---
+++ PDFs/makeLibraries.sh
@@ -0,0 +1,6 @@
+root -l -q RevCrystalBall.cxx++
+root -l -q QCD_DiJet.cxx++
+root -l -q QCD_DiJet_x.cxx++
+root -l -q GaussExp.cxx++
+root -l -q ExpGaussExp.cxx++
+root -l -q LowMassBackgroundPDF.cxx++
