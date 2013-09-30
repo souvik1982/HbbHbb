89491afd0881b26c68096ae5d49efc9461cdda08
souvik1982 <souvik1982@gmail.com>
2013-09-30 14:13:59 -0500

    LowMass_2x80_4x30_TLTL_all_TLTL

---
+++ LowMass_2x80_4x30_TLTL_all_TLTL/TLTL_nominal/processBTagging_Signal.sh
@@ -0,0 +1,3 @@
+root -l -b -q '../HbbHbb_bTagging.cc++("Signal", "glugluToX300ToHHTobbbb_8TeV_width1MeV_FASTSIM_PU", "TLTL")' 
+root -l -b -q '../HbbHbb_bTagging.cc++("Signal", "glugluToX400ToHHTobbbb_8TeV_width1MeV_FASTSIM_PU", "TLTL")' 
+say "Signal Monte Carlo with pile up done"
