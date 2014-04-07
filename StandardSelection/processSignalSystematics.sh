a43d8f7cf7ff9391b177c790c86390ca94160698
souvik1982 <souvik1982@gmail.com>
2014-04-06 20:13:11 -0500

    processSignalSystematics.sh

---
+++ StandardSelection/processSignalSystematics.sh
@@ -0,0 +1,96 @@
+source processKinSel_Signal_FULLSIM.sh
+cd MMMM_nominal
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../
+
+cd KinSel_JECp1
+source processKinSel_Signal_FULLSIM.sh
+cd MMMM_nominal
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../../
+
+cd KinSel_JECm1
+source processKinSel_Signal_FULLSIM.sh
+cd MMMM_nominal
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../../
+
+cd KinSel_JERp1
+source processKinSel_Signal_FULLSIM.sh
+cd MMMM_nominal
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../../
+
+cd KinSel_JERm1
+source processKinSel_Signal_FULLSIM.sh
+cd MMMM_nominal
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../../
+
+cd MMMM_upBC
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../
+
+cd MMMM_downBC
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../
+
+cd MMMM_upL
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../
+
+cd MMMM_downL
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../
+
+cd MMMM_upTrig
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../
+
+cd MMMM_downTrig
+source processBTagging_Signal_FULLSIM.sh
+cd a
+source processPartitioning_Signal_FULLSIM.sh
+cd ../a_KinFit
+source processPartitioning_Signal_FULLSIM.sh
+cd ../../
